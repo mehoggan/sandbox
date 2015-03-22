b769b360bda01d3ff77441ed4f4fb21a7e726437
Matthew Hoggan <mehoggan@gmail.com>
2015-03-22 13:23:49 -0700

    Removed object files from repo added to git ignore. Stoped crash.

--- c++/algorithms/morton_code/main.cpp
+++ c++/algorithms/morton_code/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     std::cout << "Input: " << std::hex << 0x000000FF << std::endl;
     morton::encoding::spread_by_one_16(0x000000FF);
     std::cout << std::endl;
-    std::cout << "Input: " << std::hex << 0x0000000000FF00FF << std::endl;
+    std::cout << "Input: " << std::hex << 0x0000000000FF00FFull << std::endl;
     morton::encoding::spread_by_one_32(0x0000000000FF00FFull);
 
     return 0;
